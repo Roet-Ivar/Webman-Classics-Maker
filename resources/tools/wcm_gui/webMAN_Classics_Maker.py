1ad6923c279d14d4b0f587998491665e0d8b8135
Niclas Jacobsson <niclas.jacobsson@istone.com>
2019-02-07 19:01:29 +0100

    Main window with icon and background

--- resources/tools/wcm_gui/webMAN_Classics_Maker.py
+++ resources/tools/wcm_gui/webMAN_Classics_Maker.py
@@ -94,5 +94,23 @@ root.iconbitmap(r'../../images/webman.ico')
 root.geometry('{}x{}'.format((main_window_width), str(main_window_height)) )
 root.resizable(width=FALSE, height=FALSE)
 
+
+
+class App:
+	def __init__(self,master):
+		frame = Frame(master)
+		master.title('webMAN Classics Maker 1.1.3 alfa')
+		master.iconbitmap(r'../../images/webman.ico') # icon upper left corner
+		master.geometry('1000x600') # Size of the window
+		frame.pack()
+		photo  = PhotoImage(file='../../images/gui_background_1000_600.gif')
+		w = Label (master, image=photo)
+		w.place(x=10, y=10, relwidth=1, relheight=1)
+		w.photo = photo
+		w.pack()
+
+
+root = Tk()
+
 app = App(root)
 root.mainloop()
