0ae8cba7703a237b9590397016c09540b6417e96
Roet-Ivar <niclasjacobsson@hotmail.com>
2019-02-15 00:53:05 +0100

    Added some more background for 1920x1080

--- resources/tools/wcm_gui/webMAN_Classics_Maker_lazy.py
+++ resources/tools/wcm_gui/webMAN_Classics_Maker_lazy.py
@@ -1,22 +1,23 @@
 from tkinter import *
-from tkinter import font
+from tkFont import Font
 
 class Main_UI():
 
 
 	def __init__(self, main):
 
-
-	
 		# canvas for image
 		self.canvas = Canvas(main, width=main_width, height=main_height, borderwidth =0, highlightthickness=0)
 		self.canvas.grid(row=0, column=0)
 
 		# images
 		self.my_images = []
-		self.my_images.append(PhotoImage(file='gui_background_1280_720_merged.gif'))
-		self.my_images.append(PhotoImage(file='gui_background_1280_720_dark.gif'))
-		self.my_images.append(PhotoImage(file='gui_background_1280_720.gif'))
+		self.my_images.append(PhotoImage(file='gui_background_1920_1080_merged.gif'))
+		self.my_images.append(PhotoImage(file='gui_background_1920_1080_dark.gif'))
+		self.my_images.append(PhotoImage(file='gui_background_1920_1080.gif'))
+		self.my_images.append(PhotoImage(file='ps3_blue_waves_1920_1080.gif'))
+		self.my_images.append(PhotoImage(file='ps3_blue_waves_2_1920_1080.gif'))
+		self.my_images.append(PhotoImage(file='ps3_blue_waves_3_1920_1080.gif'))
 		self.my_image_number = 0
 
 		# set first image on canvas
@@ -25,35 +26,47 @@ class Main_UI():
 		self.init_param_sfo_labels(main)
 
 	def init_param_sfo_labels(self, main):
+		# Constants
+		dark_side_width 		= main_width-30
+		dark_side_height 		= main_height-30
+		dark_side_panel_x_y		= [686, 19]
 
-		text_title_id 	= 'Title ID'
-		text_title		= 'Title'
-		text_iso_path	= 'ISO path'
+		dark_side_text_space	= 20
+		dark_side_padding 		= 20
 
-		height_of_text 			= font.Font(font='TkDefaultFont').metrics('linespace')
-		width_of_title_id_text 	= font.Font(size=9, family='TkDefaultFont').measure(text_title_id)
-		width_of_title_text 	= font.Font(size=9, family='TkDefaultFont').measure(text_title)
-		width_of_iso_path_text 	= font.Font(size=9, family='TkDefaultFont').measure(text_iso_path)
+		text_title_id 	= 'TITLE ID'
+		text_title		= 'TITLE'
+		text_iso_path	= 'ISO PATH'
 
-		title_id_text_x			= dark_side_panel_x_y[0] + dark_side_padding + width_of_title_id_text / 2
-		title_id_text_y 		= dark_side_panel_x_y[1] + 1 * dark_side_padding + height_of_text / 2
+		height_of_text 			= Font(font='Helvetica').metrics('linespace')
+		width_of_title_id_text 	= Font(size=15, family='Helvetica').measure(text_title_id) +1
+		width_of_title_text 	= Font(size=15, family='Helvetica').measure(text_title) +1
+		width_of_iso_path_text 	= Font(size=15, family='Helvetica').measure(text_iso_path) +1
+		
+		print('DEBUG height_of_text: ' 			+ str(height_of_text))
+		print('DEBUG width_of_title_id_text: ' 	+ str(width_of_title_id_text))
+		print('DEBUG width_of_title_text: ' 	+ str(width_of_title_text))
+		print('DEBUG width_of_iso_path_text: ' 	+ str(width_of_iso_path_text))
 
-		title_text_x			= dark_side_panel_x_y[0] + dark_side_padding + width_of_title_text / 2
-		title_text_y 			= dark_side_panel_x_y[1] + 2 * dark_side_padding + height_of_text / 2
+		title_id_text_x_pos		= dark_side_panel_x_y[0] + 1*dark_side_padding + width_of_title_id_text/2
+		title_id_text_y_pos 	= dark_side_panel_x_y[1] + 1*dark_side_padding + dark_side_padding
 
-		iso_path_text_x			= dark_side_panel_x_y[0] + dark_side_padding + width_of_iso_path_text / 2
-		iso_path_text_y 		= dark_side_panel_x_y[1] + 3 * dark_side_padding + height_of_text / 2
+		title_text_x_pos		= dark_side_panel_x_y[0] + 1*dark_side_padding + width_of_title_text/2
+		title_text_y_pos 		= title_id_text_y_pos + height_of_text + dark_side_padding
 
-		self.title_id_text_id = self.canvas.create_text(title_id_text_x, title_id_text_y, text=text_title_id, fill="White")
-		self.title_text_id = self.canvas.create_text(title_text_x, title_text_y, text=text_title, fill="White")
-		self.iso_path_text_id = self.canvas.create_text(iso_path_text_x, iso_path_text_y, text=text_iso_path, fill="White")
-		# bounds = self.canvas.bbox(self.title_id_text_id)
-		# print('bounds width: ' + str(bounds[2] - bounds[0]))
-		# print('bounds height: ' + str(bounds[3] - bounds[1]))
+		iso_path_text_x_pos		= dark_side_panel_x_y[0] + 1*dark_side_padding + width_of_iso_path_text/2
+		iso_path_text_y_pos 	= title_text_y_pos + height_of_text + dark_side_padding
+
+		self.title_id_text_id 	= self.canvas.create_text(title_id_text_x_pos, title_id_text_y_pos, 	text=text_title_id,	fill="White", font=("Helvetica", 15))
+		self.title_text_id 		= self.canvas.create_text(title_text_x_pos, 	title_text_y_pos, 		text=text_title , 	fill="White", font=("Helvetica", 15))
+		self.iso_path_text_id 	= self.canvas.create_text(iso_path_text_x_pos, iso_path_text_y_pos,	text=text_iso_path,	fill="White", font=("Helvetica", 15))
 
 		e1 = Entry(main)
-		e1.grid(row=0, column=0)
-		e1.place(x=dark_side_padding + width_of_title_id_text + dark_side_panel_x_y[0] + dark_side_padding, y=dark_side_padding + dark_side_panel_x_y[1])
+		e2 = Entry(main)
+		e3 = Entry(main)
+		e1.place(x=5*dark_side_padding + width_of_title_id_text + dark_side_panel_x_y[0], 	y=title_id_text_y_pos	-15)
+		e2.place(x=5*dark_side_padding + width_of_title_id_text	+ dark_side_panel_x_y[0], 	y=title_text_y_pos		-15)
+		e3.place(x=5*dark_side_padding + width_of_title_id_text + dark_side_panel_x_y[0], 	y=iso_path_text_y_pos	-15)
 
 	def init_main_window_buttons(self, main):
 
@@ -64,7 +77,7 @@ class Main_UI():
 
 		# button to change image
 		self.change_button = Button(main, text="Change", command=self.on_change_button, bd=1, bg="#FBFCFB")
-		self.change_button.place(x=30, y=0)
+		self.change_button.place(x=45, y=0)
 
 	def on_change_button(self):
 
@@ -79,15 +92,10 @@ class Main_UI():
 		self.canvas.itemconfig(self.image_on_canvas, image = self.my_images[self.my_image_number])
 
 
-main_width = 1280
-main_height = 720
+main_width = 1920
+main_height = 1080
 
-dark_side_width = main_width-30
-dark_side_height = main_height-30
-dark_side_panel_x_y = [686, 19]
 
-dark_side_text_space = 20
-dark_side_padding = 20
 
 main_window = Tk()
 # changing the title of our master widget
