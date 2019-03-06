1d688866ad2a1c731e0f70d776251112291444ef
Niclas Jacobsson <niclas.jacobsson@istone.com>
2019-03-06 16:31:34 +0100

    Fixed basic scaling for linux and windows

--- resources/tools/wcm_gui/webMAN_Classics_Maker_lazy.py
+++ resources/tools/wcm_gui/webMAN_Classics_Maker_lazy.py
@@ -1,6 +1,4 @@
 import json
-import sys
-import os
 
 from Tkinter import *
 from tkFont import Font
@@ -9,11 +7,6 @@ from PIL import Image
 from PIL.ImageTk import PhotoImage
 from PIL import ImageDraw
 from PIL import ImageFont
-
-# if 'linux' not in sys.platform:
-# 	import ctypes
-# 	user32 = ctypes.windll.user32
-# 	user32.SetProcessDPIAware()
 
 
 
@@ -24,7 +17,7 @@ class Main():
 
 		# canvas for image
 		self.canvas = Canvas(main, width=canvas_width, height=canvas_height, borderwidth=0, highlightthickness=0)
-		self.canvas.grid(row=0, column=0)
+		self.canvas.pack(fill=BOTH, expand=YES)
 
 		self.vcmd = main.register(self.validate)
 		self.vcmd2 = main.register(self.validate)
@@ -37,94 +30,59 @@ class Main():
 
 
 		# images
-
-		old_x = Image.open("logo_drive_hdd.gif").width
-		old_y = Image.open("logo_drive_hdd.gif").height
-		new_x = int(Image.open("logo_drive_hdd.gif").width*1.5)
-		y_scale = float(old_y) / float(old_x)
-
-		hundred_fifty = (new_x,  int(new_x * y_scale))
-
 		self.logo_drives = []
-		self.logo_drives.append(PhotoImage(Image.open("logo_drive_hdd.gif").resize(hundred_fifty)))
-		self.logo_drives.append(PhotoImage(Image.open('logo_drive_usb.gif').resize(hundred_fifty)))
+		self.logo_drives.append(PhotoImage(Image.open("logo_drive_hdd.gif")))
+		self.logo_drives.append(PhotoImage(Image.open('logo_drive_usb.gif')))
 
 		self.logo_systems = []
-		self.logo_systems.append(PhotoImage(Image.open('logo_system_PSP.gif').resize(hundred_fifty)))
-		self.logo_systems.append(PhotoImage(Image.open('logo_system_PSX.gif').resize(hundred_fifty)))
-		self.logo_systems.append(PhotoImage(Image.open('logo_system_PS2.gif').resize(hundred_fifty)))
-		self.logo_systems.append(PhotoImage(Image.open('logo_system_PS3.gif').resize(hundred_fifty)))
-
-		# test text
-		game_title_test_text = 'Burnout Revenge'
-		img = Image.open('background_light_dark_1920_1080.png')
-		draw = ImageDraw.Draw(img)
-		draw.text((850, 475), game_title_test_text, fill='white', font=ImageFont.truetype('./fonts/SCE-PS3.ttf', 25))
+		self.logo_systems.append(PhotoImage(Image.open('logo_system_PSP.gif')))
+		self.logo_systems.append(PhotoImage(Image.open('logo_system_PSX.gif')))
+		self.logo_systems.append(PhotoImage(Image.open('logo_system_PS2.gif')))
+		self.logo_systems.append(PhotoImage(Image.open('logo_system_PS3.gif')))
 
 		self.background_images = []
-		self.background_images.append((img))
-		self.background_images.append((Image.open('background_mod.png')))
-		self.background_images.append((Image.open('background_mod_2.png')))
-		self.background_images.append((Image.open('background_mod_3.png')))
-		self.background_images.append((Image.open('background_mod_3_blur.png')))
-		self.background_images.append((Image.open('background_dark_1920_1080.gif')))
-		self.background_images.append((Image.open('background_light_1920_1080.gif')))
-		self.background_images.append((Image.open('background_dark_blue_symbols_1920_1080.gif')))
-		self.background_images.append((Image.open('background_light_blue_waves_1920_1080.gif')))
-		self.background_images.append((Image.open('background_light_blue_symbols_1920_1080.gif')))
-
-		self.wallpapers = []
-		self.wallpapers.append(PhotoImage(img))
-		self.wallpapers.append(PhotoImage(Image.open('background_mod.png')))
-		self.wallpapers.append(PhotoImage(Image.open('background_mod_2.png')))
-		self.wallpapers.append(PhotoImage(Image.open('background_mod_3.png')))
-		self.wallpapers.append(PhotoImage(Image.open('background_mod_3_blur.png')))
-		self.wallpapers.append(PhotoImage(Image.open('background_dark_1920_1080.gif')))
-		self.wallpapers.append(PhotoImage(Image.open('background_light_1920_1080.gif')))
-		self.wallpapers.append(PhotoImage(Image.open('background_dark_blue_symbols_1920_1080.gif')))
-		self.wallpapers.append(PhotoImage(Image.open('background_light_blue_waves_1920_1080.gif')))
-		self.wallpapers.append(PhotoImage(Image.open('background_light_blue_symbols_1920_1080.gif')))
+		self.background_images.append(Image.open('background_light_dark_1920_1080.png'))
+		self.background_images.append(Image.open('background_mod.png'))
+		self.background_images.append(Image.open('background_mod_2.png'))
+		self.background_images.append(Image.open('background_mod_3.png'))
+		self.background_images.append(Image.open('background_mod_3_blur.png'))
+		self.background_images.append(Image.open('background_dark_1920_1080.gif'))
+		self.background_images.append(Image.open('background_light_1920_1080.gif'))
+		self.background_images.append(Image.open('background_dark_blue_symbols_1920_1080.gif'))
+		self.background_images.append(Image.open('background_light_blue_waves_1920_1080.gif'))
+		self.background_images.append(Image.open('background_light_blue_symbols_1920_1080.gif'))
 
 		self.image_xmb_icons = Image.open('XMB_icons.png')
 
-		self.image_pic1	= Image.open('../../pkg/PIC1.PNG')
-		self.image_pic0	= Image.open('../../pkg/PIC0.PNG')
+		self.image_pic1		= Image.open('../../pkg/PIC1.PNG')
+		self.image_pic0		= Image.open('../../pkg/PIC0.PNG')
 		self.image_icon0	= Image.open('../../pkg/ICON0.PNG')
-		self.image_icon0 = self.image_icon0.crop((10, 10, self.image_icon0.width - 10, self.image_icon0.height - 10))
+		self.image_icon0_crop 	= self.image_icon0.crop((10, 10, self.image_icon0.width - 10, self.image_icon0.height - 10))
+		self.ps3_system_logo = Image.open('./ps3_type_logo.png')
 
-
-
-		pic1_x_scale		= 1280.0/self.image_pic1.width
-		pic1_y_scale		= 720.0/self.image_pic1.height
-
-		# self.image_pic1.paste(self.image_icon0, (425, 500))
-		self.image_pic1.paste(self.image_xmb_icons, (0, 0), self.image_xmb_icons)
-
-		self.pic1_dimensions	= (int(self.image_pic1.width*pic1_x_scale), int(self.image_pic1.height*pic1_y_scale))
+		pic1_x_scale		= 1280.0/self.image_pic1.width * scaling
+		pic1_y_scale		= 720.0/self.image_pic1.height * scaling
 		self.pic0_dimensions 	= (int(pic1_x_scale * self.image_pic0.width), int(pic1_y_scale * self.image_pic0.height))
 		self.icon0_dimensions 	= (int(pic1_x_scale * self.image_icon0.width), int(pic1_y_scale * self.image_icon0.height))
 
+		self.pkg_images_img = []
+		self.pkg_images_img.append(self.image_pic0)
+		self.pkg_images_img.append(self.image_pic1)
+		self.pkg_images_img.append(self.image_icon0_crop)
+
+
 		game_title_test_text = 'Burnout Revenge'
 		text_color = 'white'
-		shadow_color = 'black'
 		game_text_x = 760
 		game_text_y = 490
 		text_size = 32
-		outline_amount = 2
-		# self.draw_text_on_image_w_shadow(self.image_pic1, game_title_test_text, game_text_x, game_text_y, text_size, outline_amount, text_color, shadow_color)
 		self.draw_text_on_image(self.image_pic1, game_title_test_text, game_text_x, game_text_y, text_size, text_color)
 
-
-		ps3_system_logo = Image.open('./ps3_type_logo.png')
-		self.image_pic1.paste(ps3_system_logo, (1180, 525), ps3_system_logo)
-
-		self.pkg_images = []
-		self.pkg_images.append(PhotoImage(self.image_pic0.resize(self.pic0_dimensions)))
-		self.pkg_images.append(PhotoImage(self.image_pic1.resize(self.pic1_dimensions)))
-		self.pkg_images.append(PhotoImage(self.image_icon0.resize(self.icon0_dimensions)))
+		self.image_pic1.paste(self.image_xmb_icons, (0, 0), self.image_xmb_icons)
+		self.image_pic1.paste(self.ps3_system_logo, (1180, 525), self.ps3_system_logo)
 
 
-
+		# init defintions
 		self.init_main_window_buttons(main)
 		self.init_param_sfo_labels(main)
 
@@ -136,19 +94,10 @@ class Main():
 		self.text_iso_path	= 'Path'
 
 
-		self.height_of_text = Font(font='Helvetica').metrics('linespace')
-		width_of_title_id_text = Font(size=15, family='Helvetica').measure(self.text_title_id.upper())
-		width_of_title_text = Font(size=15, family='Helvetica').measure(self.text_title.upper())
-		width_of_filename_text = Font(size=15, family='Helvetica').measure(self.text_filename.upper())
-		width_of_iso_path_text = Font(size=15, family='Helvetica').measure(self.text_iso_path.upper())
-
-		print('self.height_of_text' + str(self.height_of_text))
-		print('width_of_title_id_text' + str(width_of_title_id_text))
-		print('width_of_title_text' + str(width_of_title_text))
-		print('width_of_filename_text' + str(width_of_filename_text))
-		print('width_of_iso_path_text' + str(width_of_iso_path_text))
-
 		# paddings
+		self.height_of_text = 15 #Font(font='Helvetica').metrics('linespace')
+		print('self.height_of_text' + str(self.height_of_text))
+
 		self.dark_side_padding = 20
 		self.text_box_spacing = 8 * self.dark_side_padding
 
@@ -156,27 +105,17 @@ class Main():
 		self.main_offset_x_pos = 1400
 		self.main_offset_y_pos = 150
 
-		self.title_id_text_x_pos = self.main_offset_x_pos #+ width_of_title_id_text / 2
+		self.title_id_text_x_pos = self.main_offset_x_pos
 		self.title_id_text_y_pos = self.main_offset_y_pos + self.height_of_text / 2
 
-		self.title_text_x_pos = self.main_offset_x_pos #+ width_of_title_text / 2
+		self.title_text_x_pos = self.main_offset_x_pos
 		self.title_text_y_pos = self.dark_side_padding + self.title_id_text_y_pos + self.height_of_text
 
-		self.filename_text_x_pos = self.main_offset_x_pos #+ width_of_filename_text / 2
+		self.filename_text_x_pos = self.main_offset_x_pos
 		self.filename_text_y_pos = self.dark_side_padding + self.title_text_y_pos + self.height_of_text
 
-		self.iso_path_text_x_pos = self.main_offset_x_pos #+ width_of_iso_path_text / 2
+		self.iso_path_text_x_pos = self.main_offset_x_pos
 		self.iso_path_text_y_pos = self.dark_side_padding + self.filename_text_y_pos + self.height_of_text
-
-		# set background with label texts on canvas
-		# self.my_image_number = 0
-		# self.draw_text_on_image(self.background_images[self.my_image_number], self.text_title_id.upper(), 	self.title_id_text_x_pos,	self.title_id_text_y_pos,	25, 'white')
-		# self.draw_text_on_image(self.background_images[self.my_image_number], self.text_title.upper(), 	self.title_text_x_pos,		self.title_text_y_pos, 		25, 'white')
-		# self.draw_text_on_image(self.background_images[self.my_image_number], self.text_filename.upper(), 	self.filename_text_x_pos,	self.filename_text_y_pos,	25, 'white')
-		# self.draw_text_on_image(self.background_images[self.my_image_number], self.text_iso_path.upper(), 	self.iso_path_text_x_pos,	self.iso_path_text_y_pos,	25, 'white')
-		# self.current_background = PhotoImage(self.background_images[self.my_image_number])
-		# self.image_on_canvas = self.canvas.create_image(0, 0, anchor=NW, image=self.current_background)
-
 
 		self.my_image_number = 0
 		self.draw_text_on_image(self.background_images[self.my_image_number], self.text_title_id.upper(), 	self.title_id_text_x_pos,	self.title_id_text_y_pos,	25, 'white')
@@ -185,9 +124,9 @@ class Main():
 		self.draw_text_on_image(self.background_images[self.my_image_number], self.text_iso_path.upper(), 	self.iso_path_text_x_pos,	self.iso_path_text_y_pos,	25, 'white')
 		self.current_img = self.background_images[self.my_image_number]
 		self.current_img = self.background_images[0]
-		# linux scaling test
-		# self.current_img = self.current_img.resize((int(1920 * 0.66), int(1080 * 0.66)))
+		self.current_img = self.current_img.resize((int(1920 * scaling), int(1080 * scaling)), Image.ANTIALIAS)
 		self.current_background = PhotoImage(self.current_img)
+
 		self.image_on_canvas = self.canvas.create_image(0, 0, anchor=NW, image=self.current_background)
 
 
@@ -211,47 +150,40 @@ class Main():
 		self.button_PS2 	= Button(main, image=self.logo_systems[2], bd=1, command=lambda: self.on_drive_system_filename_choice_button(self.state_drive_choice, self.selection_system_list[2]))
 		self.button_PS3 	= Button(main, image=self.logo_systems[3], bd=1, command=lambda: self.on_drive_system_filename_choice_button(self.state_drive_choice, self.selection_system_list[3]))
 
-		# self.pkg_pic1		= Button(main, image=self.pkg_images[0], bd=1, bg="#000000")
-		self.pkg_pic0 		= Button(main, image=self.pkg_images[1], bd=1, bg="#000000")
-		self.pkg_icon0		= Button(main, image=self.pkg_images[2], bd=1, bg="#000000")
 
-		# dark side save-button
 		self.save_button = Button(main, text="Save", command=self.on_save_button, bd=1, bg="#FBFCFB")
 
 		# Placements
-		self.entry_field_title_id.place(	x=self.text_box_spacing + self.iso_path_text_x_pos, y=self.title_id_text_y_pos	, width=200)
-		self.entry_field_title.place(		x=self.text_box_spacing + self.iso_path_text_x_pos, y=self.title_text_y_pos	, width=200)
-		self.entry_field_filename.place(	x=self.text_box_spacing + self.iso_path_text_x_pos, y=self.filename_text_y_pos	, width=200)
-		self.entry_field_iso_path.place(	x=self.text_box_spacing + self.iso_path_text_x_pos, y=self.iso_path_text_y_pos	, width=200)
+		self.entry_field_title_id.place(x=int((self.text_box_spacing + self.iso_path_text_x_pos) * scaling), y=int(self.title_id_text_y_pos	* scaling)	, width=200)
+		self.entry_field_title.place(	x=int((self.text_box_spacing + self.iso_path_text_x_pos) * scaling), y=int(self.title_text_y_pos	* scaling)	, width=200)
+		self.entry_field_filename.place(x=int((self.text_box_spacing + self.iso_path_text_x_pos) * scaling), y=int(self.filename_text_y_pos	* scaling)	, width=200)
+		self.entry_field_iso_path.place(x=int((self.text_box_spacing + self.iso_path_text_x_pos) * scaling), y=int(self.iso_path_text_y_pos	* scaling)	, width=200)
 
-		self.button_HDD.place(x=self.main_offset_x_pos + 0 * 29, y=self.main_offset_y_pos - 120)
-		self.button_USB.place(x=self.main_offset_x_pos + 3 * 29, y=self.main_offset_y_pos - 120)
+		self.button_HDD.place(x=int((self.main_offset_x_pos + 0 * 29) * scaling), y=int(self.main_offset_y_pos - 120))
+		self.button_USB.place(x=int((self.main_offset_x_pos + 3 * 29) * scaling), y=int(self.main_offset_y_pos - 120))
 
-		self.button_PSP.place(	x=self.main_offset_x_pos + 0 * 29, y=self.main_offset_y_pos -80)
-		self.button_PSX.place(	x=self.main_offset_x_pos + 3 * 29, y=self.main_offset_y_pos -80)
-		self.button_PS2.place(	x=self.main_offset_x_pos + 6 * 29, y=self.main_offset_y_pos -80)
-		self.button_PS3.place(	x=self.main_offset_x_pos + 9 * 29, y=self.main_offset_y_pos -80)
+		self.button_PSP.place(x=int((self.main_offset_x_pos + 0 * 29) * scaling), y=int(self.main_offset_y_pos - 80))
+		self.button_PSX.place(x=int((self.main_offset_x_pos + 3 * 29) * scaling), y=int(self.main_offset_y_pos - 80))
+		self.button_PS2.place(x=int((self.main_offset_x_pos + 6 * 29) * scaling), y=int(self.main_offset_y_pos - 80))
+		self.button_PS3.place(x=int((self.main_offset_x_pos + 9 * 29) * scaling), y=int(self.main_offset_y_pos - 80))
 
-		self.pkg_pic1	= Button(main, image=self.pkg_images[1], highlightthickness=0, bd=0)
-		self.pkg_pic1.place(x=10, y=245)
+		self.img_pic1 = self.pkg_images_img[1]
+		self.img_pic1 = self.img_pic1.resize((int(1280 * scaling), int(720 * scaling)), Image.ANTIALIAS)
 
-		# self.pkg_pic1.place()
-		# self.pkg_pic0.place(x=250, y=300)
-		# self.pkg_pic1 = Button(main, image=self.pkg_images[1], bd=1, bg="#000000")
-		self.pkg_icon0		= Button(main, image=self.pkg_images[2], highlightthickness = 0, bd = 0)
-		self.pkg_icon0.place(x=285, y=530)
+		self.img_pic1 = PhotoImage(self.img_pic1)
+		self.pkg_pic1	= Button(main, image=self.img_pic1, highlightthickness=0, bd=0)
+		self.pkg_pic1.place(x=10 * scaling, y=245 * scaling)
+
+		self.icon0 = self.pkg_images_img[2]
+		self.icon0 = PhotoImage(self.icon0.resize(self.icon0_dimensions, Image.ANTIALIAS))
+		self.pkg_icon0		= Button(main, image=self.icon0, highlightthickness = 0, bd=0)
+
+		self.pkg_icon0.place(x=int(285 * scaling), y=int(530 * scaling))
+
 
 		icon_img = Image.open('../../pkg/ICON0.PNG')
 		self.image_pic1.paste(icon_img, (425, 450), icon_img)
 		self.image_pic1.save('test.png')
-		# test_img.paste(self.pkg_images[2], (0, 0), self.pkg_images[2])
-		if "linux" not in sys.platform:
-			print('')# os.startfile('test.png')
-
-		# self.image_pic1.paste(self.image_icon0, (425, 500))
-
-		# self.canvas.create_text(200,500,fill="white",font="Helvetica 14 italic bold",
-        #                 text="This Is A Game Title")
 
 		self.save_button.place(x=self.text_box_spacing + self.iso_path_text_x_pos, y=self.iso_path_text_y_pos + 40)
 
@@ -482,7 +414,7 @@ def dpi_awerness():
 		print(awareness.value)
 
 		# Set DPI Awareness  (Windows 10 and 8)
-		errorCode = ctypes.windll.shcore.SetProcessDpiAwareness(2)
+		errorCode = ctypes.windll.shcore.SetProcessDpiAwareness(4)
 		# the argument is the awareness level, which can be 0, 1 or 2:
 		# for 1-to-1 pixel control I seem to need it to be non-zero (I'm using level 2)
 
@@ -491,28 +423,34 @@ def dpi_awerness():
 
 		# behaviour on later OSes is undefined, although when I run it on my Windows 10 machine, it seems to work with effects identical to SetProcessDpiAwareness(1)
 
-dpi_awerness()
-
-canvas_width = 1920
-canvas_height = 1080
-
-main_window_width = 1920
-main_window_height = 1080
 
 main_window = Tk()
+main_window.resizable(0, 0)
 main_window.geometry("+%d+%d" % (0, 0))
 # changing the title of our master widget
 main_window.title('webMAN Classics Maker UI')
 # icon upper left corner
 
-if "linux" not in sys.platform:
-	print('linux')
-	main_window.iconbitmap('../../images/webman.ico')
-else:
+if "linux" in sys.platform:
 	main_window.iconbitmap('@../../images/webman_icon.xbm')
+	# scaling = 1
+	scaling = 1280.0 / 1920.0
+
+else:
+	print('not linux')
+	main_window.iconbitmap('../../images/webman.ico')
+	# scaling = 1
+	scaling = 1280.0 / 1920.0
 
 
-main_window.geometry(str(main_window_width) + 'x' + str(main_window_height))
+canvas_width = int(1920* scaling)
+canvas_height = int(1080* scaling)
+main_window_width = int(1920* scaling)
+main_window_height = int(1080* scaling)
+
+
+
+
 Main(main_window)
 
 main_window.mainloop()
