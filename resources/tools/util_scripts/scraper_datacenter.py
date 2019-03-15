1f54f2c3fec83c594ee2be3798cdfa3d243eed03
Niclas Jacobsson <niclas.jacobsson@istone.com>
2019-03-15 16:08:43 +0100

    scraper works better

--- resources/tools/util_scripts/scraper_datacenter.py
+++ resources/tools/util_scripts/scraper_datacenter.py
@@ -1,7 +1,17 @@
-from selenium import webdriver
-driver = webdriver.PhantomJS()
-driver.get('https://psxdatacenter.com/psx2/ntsc-u_list2.html')
-p_element = driver.find_element_by_id(id_='table302')
-print(p_element.text)
-# result:
-'Yay! Supports javascript'
+import json
+from bs4 import BeautifulSoup
+import requests
+# Here, we're just importing both Beautiful Soup and the Requests library
+page_link = 'https://psxdatacenter.com/psx2/ulist2.html'
+# this is the url that we've already determined is safe and legal to scrape from.
+page_response = requests.get(page_link, timeout=5)
+# here, we fetch the content from the url, using the requests library
+page_content = BeautifulSoup(page_response.content, "html.parser")
+
+table = page_content.find('table', {"id":"table302"})
+rows = table('tr')
+col1s = rows.find('td')
+
+# col1 = rows('td')
+#
+print('')
