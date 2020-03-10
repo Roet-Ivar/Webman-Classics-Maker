f49ad5799a04f65fdd81be2afe7243131d8f0c11
Niclas Jacobsson <niclas.jacobsson@istone.com>
2020-03-10 13:32:45 +0100

    Fixed an issue where webman command '/ps3.play/' is too slow to work for never versions than 1.47.14

--- resources/tools/util_scripts/edit_launch_txt.py
+++ resources/tools/util_scripts/edit_launch_txt.py
@@ -9,8 +9,12 @@ class Edit_launch_txt:
 			with open(os.path.join(AppPaths.wcm_work_dir, 'pkg.json')) as f:
 				json_data = json.load(f)
 
-			web_command = '/play.ps3'
-			web_command_string = web_command + str(json_data['iso_filepath'])
+			# web_command = '/play.ps3'
+			pre_delay = 5
+			post_delay = 2
+			pre_cmd = '/wait.ps3?' + pre_delay + ';/mount_ps3'
+			post_cmd = ';/wait.ps3?' + post_delay + ';/play.ps3'
+			web_command_string = pre_cmd + str(json_data['iso_filepath'] + post_cmd)
 			web_url_string = 'GET ' + urllib.quote(web_command_string) + ' HTTP/1.0'
 			
 			launch_txt = open(os.path.join(AppPaths.USRDIR, 'launch.txt'), 'wb')
